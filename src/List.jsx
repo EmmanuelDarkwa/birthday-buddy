3886c8afc73abb7aff599e917a0e11a481059489
Emmanuel Darkwa <emmanueldarkwaampadu@gmail.com>
2023-06-29 08:28:54 +0000

    birthday buddy commit

---
+++ src/List.jsx
@@ -0,0 +1,10 @@
+import Person from "./Person";
+
+const List = ({people}) =>{
+    return <section>
+        {people.map((person)=>{
+            return <Person key={person.key} {...person} />
+        })}
+    </section>;
+};
+export default List;
